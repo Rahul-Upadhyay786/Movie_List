7ac441c2c9d50230db0e99b29628cb554c63341c
Rahul <urrahul86@gmail.com>
2022-03-17 20:56:14 +0530

    updated

---
+++ src/Dvalues.jsx
@@ -0,0 +1,40 @@
+ const Dvalue = [
+  {
+  sname:"DARK",
+  imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+  title:" A Netflix Original Series ",
+  link:"https://www.netflix.com/in/title/80100172",
+  },
+  {
+    sname:"EXTRACTION",
+    imgsrc:"https://media-eng.dhakatribune.com/uploads/2020/04/extraction-1586268296374.jpg",
+    title:" A Netflix Original Series ",
+    link:"https://www.netflix.com/in/title/80230399",
+  },
+  {
+    sname:"EXTRA CURRICULAR",
+    imgsrc:"https://th.bing.com/th/id/OIP.lCYTiEqpEEmDWNPJTxYsGAHaEK?pid=ImgDet&rs=1",
+    title:" A Netflix Original Series ",
+    link:"https://www.netflix.com/in/title/80990668?source=35", 
+  },
+  {
+    sname:"Stranger Thing",
+    imgsrc:"https://th.bing.com/th/id/R.005ce1a3265219580297ae2b928403ba?rik=rP4bcaz0q9ZGEw&riu=http%3a%2f%2fwww.hdwallpaperslife.com%2fwp-content%2fuploads%2f2017%2f11%2fstranger_things-HD.jpg&ehk=N%2fBXbrgJY%2bnnZv%2bOCpQ0hHrFNGxuArStnfuJQLfVCJA%3d&risl=&pid=ImgRaw&r=0",
+    title:" A Netflix Original Series ",
+    link:"https://www.netflix.com/in/title/80057281",
+  },
+  {
+    sname:"Money Heist",
+    imgsrc:"https://interviewerpr.com/wp-content/uploads/2021/03/m-Cover.jpg",
+    title:" A Netflix Original Series ",
+    link:"https://www.netflix.com/in/title/80192098",
+   },
+   {
+    sname:"Vampire Diaries ",
+    imgsrc:"https://is3-ssl.mzstatic.com/image/thumb/Music69/v4/27/f8/c0/27f8c0bb-44eb-35bf-427b-fed64bfdaeec/source/3000x3000sr.jpg",
+    title:" A Netflix Original Series ",
+    link:"https://www.netflix.com/in/title/70143860",
+   }
+                    ]
+
+export default Dvalue;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import Card from "./Cards";
+import Dvalues from "./Dvalues";
+
+function netcard(val) {
+   return(
+    <Card
+    sname={val.sname}
+    imgsrc={val.imgsrc}
+    title={val.title}
+    link={val.link}
+    />
+    );
+                      }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <>
+  <h1 className='heading_style'> List Of Top 6 Netflix Series For All Time </h1>
+   {Dvalues.map(netcard)}
+   </> ,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
